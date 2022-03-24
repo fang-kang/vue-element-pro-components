56f5264fd0241c896234cdbde608eabe268a44df
fang-kang <1793980864@qq.com>
2022-03-25 00:09:50 +0800

    docs: update docs

--- src/directive/clipboard/index.js
+++ src/directive/clipboard/index.js
@@ -4,9 +4,9 @@ const install = function(Vue) {
   Vue.directive('Clipboard', Clipboard)
 }
 
-if (window.Vue) {
-  window.clipboard = Clipboard
-  Vue.use(install); // eslint-disable-line
+if (globalThis && globalThis.Vue) {
+  globalThis.clipboard = Clipboard
+  Vue.use(install) // eslint-disable-line
 }
 
 Clipboard.install = install
